04e70fb9acda771d2fac9acede6908386a22937e
Filip Mackiewicz <47855738+filipmackiewicz@users.noreply.github.com>
2022-07-12 13:05:00 +0200

    add project structure and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './modules/home/Home';
+import Candidates from './modules/candidates/Candidates';
+import Projects from './modules/projects/Projects';
+import Profile from './modules/profile/Profile';
+
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="candidates" element={<Candidates />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="projects" element={<Projects />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/modules/candidates/Candidates.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Candidates() {
+    return (
+      <div>
+        <h1>This is the candidates page</h1>
+      </div>
+    );
+  }
+  
+export default Candidates;
