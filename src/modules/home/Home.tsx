15a705f7160c2366abb3aaf22bf5f4427f78825f
Filip Mackiewicz <47855738+filipmackiewicz@users.noreply.github.com>
2022-07-12 13:48:58 +0200

    added sidebar (#3)
    * added sidebar
    
    * removed comments

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Sidebar from "../common/components/sidebar/Sidebar";
 
 function Home() {
     return (
       <div>
         <h1>This is the home page</h1>
+        <Sidebar />
       </div>
     );
   }
