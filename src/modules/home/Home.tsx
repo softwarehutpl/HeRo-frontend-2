13df5f040ca06b4504cb55ca611d2191980c6d5b
GrzegorzS <gszeszko30@gmail.com>
2022-07-12 22:21:27 +0200

    Adding InfoBox to Home page

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -1,13 +1,22 @@
-import React from "react";
 import Navbar from "../common/components/navbar/Navbar";
 import Sidebar from "../common/components/sidebar/Sidebar";
 import { Outlet } from "react-router-dom";
+import Box from "@mui/material/Box";
+import InfoBox from "../common/components/infobox/InfoBox";
 
 function Home() {
   return (
     <div>
       <Navbar />
       <Sidebar />
+      <Box sx={{
+        display: "flex",
+        justifyContent: "flex-start"
+      }}>
+        <InfoBox title="New Candidates" value={5} name="Show"/>
+        <InfoBox title="Interviews this week" value={8} name="Show"/>
+        <InfoBox title="New vodkas this month" value={4} name="Show"/>
+      </Box>
       <Outlet />
     </div>
   );
