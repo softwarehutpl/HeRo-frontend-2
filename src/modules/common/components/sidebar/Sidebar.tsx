b19d9da8b13447cb2686159a64034469c0f60dfb
Filip Mackiewicz <47855738+filipmackiewicz@users.noreply.github.com>
2022-07-12 13:48:01 +0200

    removed comments

--- src/modules/common/components/sidebar/Sidebar.tsx
+++ src/modules/common/components/sidebar/Sidebar.tsx
@@ -3,16 +3,13 @@ import MenuList from '@mui/material/MenuList';
 import MenuItem from '@mui/material/MenuItem';
 import Paper from '@mui/material/Paper';
 import ListItemIcon from '@mui/material/ListItemIcon';
-// import DraftsIcon from '@mui/icons-material/Drafts';
-// import SendIcon from '@mui/icons-material/Send';
-// import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
 import HomeIcon from '@mui/icons-material/Home';
 import PeopleIcon from '@mui/icons-material/People';
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 
 export default function Sidebar() {
   return (
-    <Paper elevation={0} sx={{ width: 50, height: 400 }}>
+    <Paper elevation={0} sx={{ width: 50, height: 300 }}>
       <MenuList>
         <MenuItem>
           <ListItemIcon>
