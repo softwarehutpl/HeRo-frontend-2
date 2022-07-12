a7c814c7b355ad2b867f61828e5cda8e40bf4ee6
Jakub Sosnowski <jakub.diversa@wp.pl>
2022-07-12 13:44:30 +0200

    Adding navbar

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Navbar from "../common/components/navbar/Navbar";
 
 function Home() {
-    return (
-      <div>
-        <h1>This is the home page</h1>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
 export default Home;
