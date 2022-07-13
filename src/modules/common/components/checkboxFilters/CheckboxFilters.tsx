0155e845b791dda813074b3cc31afe4ae1adc2c6
JakubSosn <82612227+JakubSosn@users.noreply.github.com>
2022-07-13 14:09:45 +0200

    Checkbox filter (#18)
    * changing navbar color for black
    
    * added row component
    
    * added row component
    
    * added row component
    
    * added row component
    
    * fix checked funcion
    
    * fix checkbox funcion

--- src/modules/common/components/checkboxFilters/CheckboxFilters.tsx
+++ src/modules/common/components/checkboxFilters/CheckboxFilters.tsx
@@ -3,16 +3,22 @@ import FormGroup from "@mui/material/FormGroup";
 import CheckboxRow from "./CheckboxRow";
 
 const CheckboxFilters = () => {
-  const [checked, setChecked] = React.useState(true);
 
-  const handleCheckedbox = () => {
-    setChecked((prevState) => !prevState);
+  const [checked, setChecked] = React.useState(false);
+
+  const handleOnClick = (value: boolean) => {
+    setChecked(value);
+    // go to parent
   };
 
   return (
     <FormGroup>
       <h3>Status</h3>
-      <CheckboxRow isChecked={checked} label="New" />
+      <CheckboxRow
+        isChecked={checked}
+        handleOnClick={handleOnClick}
+        label="New"
+      />
     </FormGroup>
   );
 };
--- src/modules/common/components/checkboxFilters/CheckboxRow.tsx
+++ src/modules/common/components/checkboxFilters/CheckboxRow.tsx
@@ -1,19 +1,29 @@
-import react, { FunctionComponent } from "react";
+
+import { FunctionComponent, useState } from "react";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 
 interface CheckboxRowProps {
   label: string;
   isChecked: boolean;
+  handleOnClick: (value: boolean) => void;
 }
 
 const CheckboxRow: FunctionComponent<CheckboxRowProps> = ({
   label,
   isChecked,
+  handleOnClick,
 }) => {
+  const [checkboxValue, setCheckboxValue] = useState(isChecked);
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCheckboxValue(event.target.checked);
+    handleOnClick(event.target.checked);
+  };
+
   return (
     <FormControlLabel
-      control={<Checkbox checked={isChecked} />}
+      control={<Checkbox checked={checkboxValue} onChange={handleOnChange} />}
       label={label}
     />
   );
