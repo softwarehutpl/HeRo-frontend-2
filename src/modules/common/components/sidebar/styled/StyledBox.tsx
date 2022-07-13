e15984a6052070c2cbe556a677d08035760b6066
Filip Mackiewicz <47855738+filipmackiewicz@users.noreply.github.com>
2022-07-13 11:17:26 +0200

    colors changed and add paths to links (#14)

--- src/modules/common/components/sidebar/styled/StyledBox.tsx
+++ src/modules/common/components/sidebar/styled/StyledBox.tsx
@@ -4,7 +4,7 @@ import { styled } from '@mui/material/styles';
 export const StyledBox = styled(Box)`
   max-width: 50px;
   min-height: calc(100vh - 63.99px);
-  background-color: #F4F4F4;
+  background-color: #FFFFFF;
   margin-left: 0px;
   margin-right: 2px;
 `;
