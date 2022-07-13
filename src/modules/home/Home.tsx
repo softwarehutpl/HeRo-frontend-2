c7f0207c7b5810396d7ca6e230911311e5fd28a7
Filip Mackiewicz <47855738+filipmackiewicz@users.noreply.github.com>
2022-07-13 10:50:32 +0200

    changes styles and routing

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -1,26 +1,14 @@
-import Navbar from "../common/components/navbar/Navbar";
-import Sidebar from "../common/components/sidebar/Sidebar";
-import { Outlet } from "react-router-dom";
-import Box from "@mui/material/Box";
 import InfoBox from "../common/components/infobox/InfoBox";
-import { styled } from '@mui/material/styles';
-
-
-
-const InfoBoxWrapper = styled(Box)`
-  display: flex;
-  justify-content: flex-start;
-`;
-
+import { StyledBox } from "./styled/StyledBox";
 
 
 function Home() {
   return (
-        <InfoBoxWrapper >
+        <StyledBox >
           <InfoBox title="New Candidates" value={5} name="Show"/>
           <InfoBox title="Interviews this week" value={8} name="Show"/>
           <InfoBox title="New vodkas this month" value={4} name="Show"/>
-        </InfoBoxWrapper>
+        </StyledBox>
   );
 }
 
