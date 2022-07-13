ad628296d0a01777278e3cf4a3049329ae5e765d
GrzegorzSzeszko <108670423+GrzegorzSzeszko@users.noreply.github.com>
2022-07-13 11:17:59 +0200

    fixing infobox style (#15)

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -5,9 +5,8 @@ import { StyledBox } from "./styled/StyledBox";
 function Home() {
   return (
         <StyledBox >
-          <InfoBox title="New Candidates" value={5} name="Show"/>
-          <InfoBox title="Interviews this week" value={8} name="Show"/>
-          <InfoBox title="New vodkas this month" value={4} name="Show"/>
+          <InfoBox title="New Candidates" value={5} name="Show" route="/candidates"/>
+          <InfoBox title="Interviews this week" value={8} name="Show" route="/projects"/>
         </StyledBox>
   );
 }
