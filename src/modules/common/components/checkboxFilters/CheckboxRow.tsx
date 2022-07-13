aef5b11496ebe475ff955d01acca68e6570a0bc7
Jakub Sosnowski <jakub.diversa@wp.pl>
2022-07-13 10:48:18 +0200

    added row component

---
+++ src/modules/common/components/checkboxFilters/CheckboxRow.tsx
@@ -0,0 +1,22 @@
+import { FunctionComponent } from "react";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+
+interface CheckboxRowProps {
+  label: string;
+  isChecked: boolean;
+}
+
+const CheckboxRow: FunctionComponent<CheckboxRowProps> = ({
+  label,
+  isChecked,
+}) => {
+  return (
+    <FormControlLabel
+      control={<Checkbox checked={isChecked} />}
+      label={label}
+    />
+  );
+};
+
+export default CheckboxRow;
