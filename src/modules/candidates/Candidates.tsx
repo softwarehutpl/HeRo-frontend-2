29f9711c274f364f6ed0e179b843ccd3efcccd13
GrzegorzSzeszko <108670423+GrzegorzSzeszko@users.noreply.github.com>
2022-07-14 13:07:49 +0200

    Finishing candidate table context (#35)
    * adding styles folder, file
    
    * finished context tabel/kanban

--- src/modules/candidates/Candidates.tsx
+++ src/modules/candidates/Candidates.tsx
@@ -3,20 +3,17 @@ import CheckboxFilters from "../common/components/checkboxFilters/CheckboxFilter
 import StickyHeadTable from "../common/components/table/StickyHeadTable";
 import { CustomDiv } from "./CandidatesStyles";
 import CandidatesTableHeader from "./headerCandidates/HeaderCandidates";
+import {useTables } from "../contexts/CandidatesTablesContext"
 
 function Candidates() {
+  const whichTable = useTables().value;
   return (
     <CustomDiv>
       <CheckboxFilters />
-      <div>
-        <CandidatesTableHeader
-          title="Candidates"
-          routeOne="/candidates"
-          routeTwo="/projects"
-        />
-        <StickyHeadTable />
-        <div>Kaban</div>
-      </div>
+        <div>
+         <CandidatesTableHeader title="Candidates" routeOne="/candidates" routeTwo="/projects"/>
+            {whichTable === 0 ? <StickyHeadTable /> : <div style={{ fontSize: "200px" }}>Kaban</div>}
+        </div>
     </CustomDiv>
   );
 }
