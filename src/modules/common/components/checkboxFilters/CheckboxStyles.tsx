411c2a38f5531a4c0e1c287ef92359e51d5f1813
JakubSosn <82612227+JakubSosn@users.noreply.github.com>
2022-07-14 16:33:15 +0200

    change checkboxFilter function (#43)

--- src/modules/common/components/checkboxFilters/CheckboxStyles.tsx
+++ src/modules/common/components/checkboxFilters/CheckboxStyles.tsx
@@ -1,16 +1,15 @@
-
-import { styled } from '@mui/material/styles';
+import { styled } from "@mui/material/styles";
 import FormGroup from "@mui/material/FormGroup";
 
-export const StyledHeading = styled('h2')`
-    margin: 0;
-    padding-top: 10px;
-    font-size: 20px;
+export const StyledHeading = styled("h2")`
+  margin: 0;
+  padding-top: 10px;
+  font-size: 20px;
 `;
 
 export const CustomFormGroup = styled(FormGroup)`
-    min-width: 150px;
-    padding-left: 10px;
-    border-left: solid 1px #C5C5C5;
-    border-right: solid 1px #C5C5C5;
+  min-width: 150px;
+  padding-left: 10px;
+  border-left: solid 1px #c5c5c5;
+  border-right: solid 1px #c5c5c5;
 `;
