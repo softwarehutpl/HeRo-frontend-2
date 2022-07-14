00e2caefa9be285bdfc9b0c31c578a1aeff1d921
GrzegorzS <gszeszko30@gmail.com>
2022-07-14 10:40:48 +0200

    adding not finished context to tables

---
+++ src/modules/contexts/CandidatesTablesContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, useContext, useState, FC} from "react";
+
+
+enum CandidatesDisplayMode {
+  TABLE,
+  KABAN
+}
+
+interface TablesCOntextInterface {
+  value: CandidatesDisplayMode;
+  setValue: (value: CandidatesDisplayMode) => void;
+}
+
+type Props = {
+  children: JSX.Element,
+};
+
+
+const TablesContext = createContext<TablesCOntextInterface>({
+  value: CandidatesDisplayMode.TABLE,
+  setValue: () => {}
+});
+
+
+
+
+const TablesProvider: FC<Props>= ( {children} ) => {
+  const [value, setValue] = useState<CandidatesDisplayMode>(CandidatesDisplayMode.TABLE);
+
+
+  return (
+    <TablesContext.Provider value={{value, setValue}}>
+      {children}
+    </TablesContext.Provider>
+  );
+};
+
+
+export default TablesProvider;
+
+export const useTables = () => useContext(TablesContext);
