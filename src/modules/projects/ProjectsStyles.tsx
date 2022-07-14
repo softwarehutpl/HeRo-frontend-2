79e8c5a2384f5ac53d49db1cfd784920991cdf6c
GrzegorzSzeszko <108670423+GrzegorzSzeszko@users.noreply.github.com>
2022-07-14 16:22:47 +0200

    Fixing projects display (#42)
    * adding styles folder, file
    
    * fixing display

--- src/modules/projects/ProjectsStyles.tsx
+++ src/modules/projects/ProjectsStyles.tsx
@@ -2,7 +2,7 @@ import { styled } from "@mui/material/styles";
 
 export const CustomDiv = styled('div')`
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     width: 90%;
 `;
 
