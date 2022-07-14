20e16e88208bb79ae84edb0a55064a71175651eb
Jakub Sosnowski <jakub.diversa@wp.pl>
2022-07-14 09:37:46 +0200

    added table fo project component

--- src/modules/projects/Projects.tsx
+++ src/modules/projects/Projects.tsx
@@ -1,11 +1,61 @@
-import React from "react";
+import * as React from "react";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 
-function Projects() {
-    return (
-      <div>
-        <h1>This is the projects page</h1>
-      </div>
-    );
-  }
-  
-export default Projects;
+const columns: GridColDef[] = [
+  { field: "name", headerName: "Name", width: 200 },
+  { field: "creator", headerName: "Creator", width: 200 },
+  { field: "from", headerName: "Frome", width: 160 },
+  {
+    field: "to",
+    headerName: "To",
+    width: 160,
+    // valueGetter: (params: GridValueGetterParams) =>
+    //   `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  },
+  { field: "resume", headerName: "Resume", width: 120 },
+  { field: "hired", headerName: "Hired", width: 120 },
+];
+
+const rows = [
+  {
+    id: "1",
+    name: "JavaScript Developer",
+    creator: "Jon",
+    from: "01.07.2022",
+    to: "30.07.2022",
+    field: "14",
+    hired: "4",
+  },
+  {
+    id: "2",
+    name: "React Developer",
+    creator: "Jon",
+    from: "01.07.2022",
+    to: "30.07.2022",
+    field: "3",
+    hired: "2",
+  },
+  {
+    id: "3",
+    name: "Angular Developer",
+    creator: "Jon",
+    from: "01.07.2022",
+    to: "30.07.2022",
+    field: "1",
+    hired: "0",
+  },
+];
+
+export default function Projects() {
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        // checkboxSelection
+      />
+    </div>
+  );
+}
