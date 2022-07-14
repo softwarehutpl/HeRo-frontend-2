63f75f1122a820fb4a5b36ed80a19f5fd5fa7440
Jakub Sosnowski <jakub.diversa@wp.pl>
2022-07-14 15:09:09 +0200

    added eventList data

--- src/modules/home/eventsList.tsx
+++ src/modules/home/eventsList.tsx
@@ -1,32 +1,25 @@
 const eventList = [
   {
-    title: "All Day Event very long title",
-    allDay: true,
-    start: new Date(2022, 7, 2),
-    end: new Date(2022, 7, 3),
+    title: "Interview with John Doe",
+    start: new Date(2022, 6, 15, 9, 30),
+    end: new Date(2022, 6, 15, 10),
   },
   {
-    title: "Long Event",
-    start: new Date(2015, 3, 7),
-    end: new Date(2015, 3, 10),
-  },
-
-  {
-    title: "DTS STARTS",
-    start: new Date(2016, 2, 13, 0, 0, 0),
-    end: new Date(2016, 2, 20, 0, 0, 0),
+    title: "Phone interview with John Doe",
+    start: new Date(2022, 6, 16, 10, 30),
+    end: new Date(2022, 6, 16, 11, 30),
   },
 
   {
-    title: "DTS ENDS",
-    start: new Date(2016, 10, 6, 0, 0, 0),
-    end: new Date(2016, 10, 13, 0, 0, 0),
+    title: "Tech interview with John Doe",
+    start: new Date(2022, 6, 18, 14, 30),
+    end: new Date(2022, 6, 18, 15, 30),
   },
 
   {
-    title: "Some Event",
-    start: new Date(2015, 3, 9, 0, 0, 0),
-    end: new Date(2015, 3, 9, 0, 0, 0),
+    title: "Offer",
+    start: new Date(2022, 6, 18, 8, 30),
+    end: new Date(2022, 6, 18, 9, 30),
   },
 ];
 
