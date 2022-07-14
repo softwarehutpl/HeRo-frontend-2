2978d477dca2d060461d5ef3768119ef4d908565
JakubSosn <82612227+JakubSosn@users.noreply.github.com>
2022-07-14 14:32:44 +0200

    added calendar to home (#36)

---
+++ src/modules/home/MyCalendar.tsx
@@ -0,0 +1,21 @@
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import eventsList from "./eventsList";
+
+// Setup the localizer by providing the moment (or globalize, or Luxon) Object
+// to the correct localizer.
+const localizer = momentLocalizer(moment); // or globalizeLocalizer
+
+const MyCalendar = () => (
+  <div className="myCustomHeight">
+    <Calendar
+      localizer={localizer}
+      events={eventsList}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 600 }}
+    />
+  </div>
+);
+
+export default MyCalendar;
