a1134b866e43ba5c4ab4431fbe1d6fd6454fc9a4
Jakub Sosnowski <jakub.diversa@wp.pl>
2022-07-14 14:31:21 +0200

    added calendar to home

---
+++ src/modules/home/eventsList.tsx
@@ -0,0 +1,33 @@
+const eventList = [
+  {
+    title: "All Day Event very long title",
+    allDay: true,
+    start: new Date(2022, 7, 2),
+    end: new Date(2022, 7, 3),
+  },
+  {
+    title: "Long Event",
+    start: new Date(2015, 3, 7),
+    end: new Date(2015, 3, 10),
+  },
+
+  {
+    title: "DTS STARTS",
+    start: new Date(2016, 2, 13, 0, 0, 0),
+    end: new Date(2016, 2, 20, 0, 0, 0),
+  },
+
+  {
+    title: "DTS ENDS",
+    start: new Date(2016, 10, 6, 0, 0, 0),
+    end: new Date(2016, 10, 13, 0, 0, 0),
+  },
+
+  {
+    title: "Some Event",
+    start: new Date(2015, 3, 9, 0, 0, 0),
+    end: new Date(2015, 3, 9, 0, 0, 0),
+  },
+];
+
+export default eventList;
--- src/modules/home/styled/StyledBox.tsx
+++ src/modules/home/styled/StyledBox.tsx
@@ -1,7 +1,14 @@
-import { Box } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
 export const StyledBox = styled(Box)`
   display: flex;
+  justify-content: flex-start;
+  padding-bottom: 15px;
+`;
+
+export const MainBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
   justify-content: flex-start;
 `;
