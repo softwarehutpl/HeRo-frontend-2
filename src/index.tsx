327bd7b565243c9a6a43ba1030823c58e347776f
Filip Mackiewicz <47855738+filipmackiewicz@users.noreply.github.com>
2022-07-14 10:48:41 +0200

    added avatar

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,6 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
   <BrowserRouter>
     <App />
   </BrowserRouter>
---
+++ src/modules/common/components/avatars/CustomAvatar.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+
+
+function stringAvatar(name: string) {
+  return {
+    sx: {
+      bgcolor: '#dd0000',
+    },
+    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+  };
+}
+
+export default function CustomAvatar() {
+  return (
+      <Avatar {...stringAvatar('Tim Neutkens')} />
+  );
+}
