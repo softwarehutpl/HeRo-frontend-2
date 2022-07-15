fc48641e13f7dad109bb052151b4a183b7483d83
Jakub Sosnowski <jakub.diversa@wp.pl>
2022-07-15 14:26:53 +0200

    added in project hired link

--- src/modules/candidates/Candidates.tsx
+++ src/modules/candidates/Candidates.tsx
@@ -4,6 +4,7 @@ import StickyHeadTable from "../common/components/table/StickyHeadTable";
 import { CustomDiv, CheckboxDiv } from "./CandidatesStyles";
 import CandidatesTableHeader from "./headerCandidates/HeaderCandidates";
 import { useTables } from "../contexts/CandidatesTablesContext";
+import { Link } from "react-router-dom";
 
 function Candidates() {
   const whichTable = useTables().value;
@@ -16,6 +17,33 @@ function Candidates() {
     "Tech interview",
     "Offer",
   ];
+
+  // const Candidate = () => {
+  //   const candidateId = ["1", "2", "3"]
+  //   return (
+  //     <CustomDiv>
+  //       <CheckboxDiv>
+  //         <CheckboxFilters header="Status" filtersList={statusesList} />
+  //         <CheckboxFilters header="Stage" filtersList={stageList} />
+  //       </CheckboxDiv>
+  //       <div style={{ width: "100%" }}>
+  //         <CandidatesTableHeader title="Candidates" />
+  //         {whichTable === 0 ? (
+  //           candidateId.map(id => {
+  //             return (
+  //               <Link to={`/candidates/${id}`}>
+  //               </Link>
+  //             )
+  //             <StickyHeadTable />
+  //           })
+  //         ) : (
+  //           <div style={{ fontSize: "200px" }}>Kaban</div>
+  //         )}
+  //       </div>
+  //     </CustomDiv>
+  //   )
+  // }
+
   return (
     <CustomDiv>
       <CheckboxDiv>
