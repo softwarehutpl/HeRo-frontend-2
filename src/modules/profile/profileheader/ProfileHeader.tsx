4b06c1761f299a0b77c50cef1c65f4fbda0c6ee0
Filip Mackiewicz <47855738+filipmackiewicz@users.noreply.github.com>
2022-07-25 14:19:17 +0200

    candidate profile view

--- src/modules/profile/profileheader/ProfileHeader.tsx
+++ src/modules/profile/profileheader/ProfileHeader.tsx
@@ -1,14 +1,26 @@
+import { CoPresentOutlined } from '@mui/icons-material';
 import React from 'react';
 import CustomAvatar from '../../common/components/avatars/CustomAvatar';
 import { ProfileHeaderHeading, ProfileHeaderWrapper } from './ProfileHeaderStyles';
 
-function ProfileHeader() {
+type Candidate = {
+  candidate: {
+    id: number;
+    fullName: string;
+    email: string;
+    phoneNumber: string;
+    availableFrom: Date;
+    expectedMonthlySalary: number;
+    otherExpectations: string;
+    cvPath: string;
+  }[];
+};
+
+export const ProfileHeader: React.FC<Candidate> = props => {
   return (
     <ProfileHeaderWrapper>
-      <CustomAvatar name={'John Doe'} />
-      <ProfileHeaderHeading>Profile of John Doe</ProfileHeaderHeading>
+      <CustomAvatar name={'test test'} />
+      <ProfileHeaderHeading>Profile of {props.candidate.id}</ProfileHeaderHeading>
     </ProfileHeaderWrapper>
   );
-}
-
-export default ProfileHeader;
+};
--- src/modules/profile/profileinfo/ProfileInfo.tsx
+++ src/modules/profile/profileinfo/ProfileInfo.tsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import CandidatesService from '../../common/Api/Candidates.service';
 import EvaluationHr from '../evaluationhr/EvaluationHr';
 import EvaluationTech from '../evaluationtech/EvaluationTech';
-import ProfileHeader from '../profileheader/ProfileHeader';
+import { ProfileHeader } from '../profileheader/ProfileHeader';
 import ProfileNotes from '../profilenotes/ProfileNotes';
 import ProfileSummary from '../profilesummary/ProfileSummary';
 import { ProfileInfoWrapper } from './ProfileInfoStyles';
 
+interface Candidate {
+  id: number;
+  fullName: string;
+  email: string;
+  phoneNumber: string;
+  availableFrom: Date;
+  expectedMonthlySalary: number;
+  otherExpectations: string;
+  cvPath: string;
+}
+
 function ProfileInfo() {
+  const params = useParams();
+  const [candidate, setCandidate] = useState<Candidate[]>([]);
+
+  useEffect(() => {
+    const profileData = CandidatesService.candidateHttpGet(`Get/${params.id}`);
+    profileData.then(res => setCandidate(res));
+  }, []);
+
+  console.log(candidate);
+
   return (
     <ProfileInfoWrapper>
-      <ProfileHeader />
+      <ProfileHeader candidate={candidate} />
       <ProfileSummary />
       <ProfileNotes />
       <EvaluationHr />
