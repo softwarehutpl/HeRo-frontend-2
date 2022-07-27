4f900682591b70cf247c8047482a4fcc6c92048a
Filip Mackiewicz <47855738+filipmackiewicz@users.noreply.github.com>
2022-07-27 15:02:21 +0200

    modal change

--- src/modules/projects/editdatabutton/EditDataButton.tsx
+++ src/modules/projects/editdatabutton/EditDataButton.tsx
@@ -1,45 +1,18 @@
-import { Box, IconButton, Modal, Typography } from '@mui/material';
+import { IconButton } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import React from 'react';
-import ModalForm from '../modalForm/ModalForm';
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '40%',
-  height: '90%',
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-  overflowY: 'scroll',
-};
+import { ProjectModal } from '../projectmodal/ProjectModal';
 
 export const EditDataButton = ({ index }: any) => {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState<boolean>(false);
   const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
 
   return (
     <div>
       <IconButton onClick={handleOpen}>
         <EditIcon style={{ color: '#1976d2' }}></EditIcon>
       </IconButton>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Edit project
-          </Typography>
-          <ModalForm />
-        </Box>
-      </Modal>
+      <ProjectModal status={open} setstatus={setOpen} />
     </div>
   );
 };
