4b80a25a2669dd187396a2f0b248c03a23e5af7b
Jakub Sosnowski <jakub.diversa@wp.pl>
2022-07-27 14:13:53 +0200

    added api filter projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import Provider from './provider/Provider';
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import Provider from "./provider/Provider";
+import { Routes, Route } from "react-router-dom";
 
-import Candidates from './modules/candidates/Candidates';
-import Projects from './modules/projects/Projects';
-import Profile from './modules/profile/Profile';
-import Wrapper from './modules/wrapper/Wrapper';
-import Home from './modules/home/Home';
-import SignIn from './modules/signin/Signin';
+import Candidates from "./modules/candidates/Candidates";
+import Projects from "./modules/projects/Projects";
+import Profile from "./modules/profile/Profile";
+import Wrapper from "./modules/wrapper/Wrapper";
+import Home from "./modules/home/Home";
+import SignIn from "./modules/signin/Signin";
 
 function App() {
   return (
@@ -17,7 +17,8 @@ function App() {
           <Route path="signin" element={<SignIn />} />
           <Route path="/" element={<Wrapper />}>
             <Route index element={<Home />} />
-            <Route path="candidates" element={<Candidates />} />
+            <Route path="candidates/" element={<Candidates />} />
+            <Route path="candidates/:id?/:status" element={<Candidates />} />
             <Route path="profile/:id" element={<Profile />} />
             <Route path="projects" element={<Projects />} />
           </Route>
