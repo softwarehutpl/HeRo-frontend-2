a7d23fff55dad07cffc568afde8ba5e80c698860
Jakub Sosnowski <jakub.diversa@wp.pl>
2022-07-29 09:45:39 +0200

    big change in checkbox

--- src/modules/candidates/Candidates.tsx
+++ src/modules/candidates/Candidates.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import CheckboxFilters from "../common/components/checkboxFilters/CheckboxFilters";
 import { CustomDiv, CheckboxDiv } from "./CandidatesStyles";
 import CandidatesTableHeader from "./headerCandidates/HeaderCandidates";
@@ -26,7 +26,8 @@ function Candidates() {
   const [searchParams] = useSearchParams();
   const id = searchParams.get("id") || undefined;
   const status = searchParams.get("status") || undefined;
-  console.log(id, status);
+
+  console.log(searchParams);
 
   interface CandidateGetListDto {
     paging: {
@@ -37,6 +38,96 @@ function Candidates() {
     status?: string[];
     recruitmentId?: number;
   }
+
+  interface CheckboxList {
+    id: number;
+    title: string;
+    isChecked: boolean;
+  }
+
+  type Action = {
+    id: number;
+    type: "IsChecked";
+  };
+
+  const initialCheckboxStatus: CheckboxList[] = [
+    {
+      id: 1,
+      title: "New",
+      isChecked: true,
+    },
+    {
+      id: 2,
+      title: "In Processing",
+      isChecked: true,
+    },
+    {
+      id: 3,
+      title: "Dropped out",
+      isChecked: true,
+    },
+    {
+      id: 4,
+      title: "Hired",
+      isChecked: true,
+    },
+  ];
+  const initialCheckboxStage: CheckboxList[] = [
+    {
+      id: 1,
+      title: "Evaluation",
+      isChecked: true,
+    },
+    {
+      id: 2,
+      title: "Interview",
+      isChecked: true,
+    },
+    {
+      id: 3,
+      title: "Phone interview",
+      isChecked: true,
+    },
+    {
+      id: 4,
+      title: "Tech  interview",
+      isChecked: true,
+    },
+    {
+      id: 5,
+      title: "Offer",
+      isChecked: true,
+    },
+  ];
+
+  const reducer = (state: CheckboxList[], action: Action) => {
+    switch (action.type) {
+      case "IsChecked":
+        return state.map((checked: CheckboxList) => {
+          if (checked.id == action.id) {
+            return { ...checked, isChecked: !checked.isChecked };
+          } else {
+            return checked;
+          }
+        });
+      default:
+        return state;
+    }
+  };
+
+  const handleComplete = (checkbox: any) => {
+    dispatch({ type: "IsChecked", id: checkbox });
+  };
+  const handleCompleteStage = (checkbox: any) => {
+    dispatchStage({ type: "IsChecked", id: checkbox });
+  };
+
+  const [checkboxStatus, dispatch] = useReducer(reducer, initialCheckboxStatus);
+  const [checkboxStage, dispatchStage] = useReducer(
+    reducer,
+    initialCheckboxStage
+  );
+
   const [candidateInfoForListDTOs, setcandidateInfoForListDTOs] = useState<
     Candidates[]
   >([]);
@@ -64,21 +155,39 @@ function Candidates() {
     setcandidateInfoForListDTOs(responseData.data.candidateInfoForListDTOs);
   };
 
+  if (checkboxStatus[0].isChecked) {
+    const urlParams = searchParams.append("status", "New");
+    console.log(urlParams);
+  }
   useEffect(() => {
     candidateData(id, status);
   }, [id, status]);
 
-  console.log(candidateInfoForListDTOs);
+  // if (checkboxStatus[0].isChecked) {
+  //   const urlParams = useParams();
+  //   console.log(urlParams);
+  // }
 
-  const statusesList = ["New", "In processing", "Dropped out", "Hired"];
-  const stageList = [
-    "Evaluation",
-    "Interview",
-    "Phone interview",
-    "Tech interview",
-    "Offer",
-  ];
-  const [isChecked, setIsChecked] = useState<boolean>(true);
+  const newAskData = {
+    status: [checkboxStatus[3].title],
+    paging: {
+      pageSize: 104,
+      pageNumber: 1,
+    },
+  };
+
+  const testNew = async () => {
+    const updateData = await CandidatesService.candidateHttpPost(
+      "GetList",
+      newAskData
+    );
+    console.log(updateData);
+  };
+
+  useEffect(() => {
+    // console.log(`zmiana checkboxa new ${checkboxStatus[0]}`);
+    testNew();
+  }, [checkboxStatus[0]]);
 
   return (
     <>
@@ -87,15 +196,13 @@ function Candidates() {
           <CheckboxDiv>
             <CheckboxFilters
               header="Status"
-              filtersList={statusesList}
-              isChecked={isChecked}
-              setIsChecked={setIsChecked}
+              checkbox={checkboxStatus}
+              dispatch={handleComplete}
             />
             <CheckboxFilters
               header="Stage"
-              filtersList={stageList}
-              isChecked={isChecked}
-              setIsChecked={setIsChecked}
+              checkbox={checkboxStage}
+              dispatch={handleCompleteStage}
             />
           </CheckboxDiv>
           <div style={{ width: "100%" }}>
